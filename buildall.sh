e2cad32ca2c75db5ccba1bcc4fe65fec12bea725
cresny <cresnick@mediamath.com>
2018-10-11 14:20:45 +0000

    support KUDU_VERSION override

--- buildall.sh
+++ buildall.sh
@@ -294,7 +294,7 @@ FLATBUFFERS_VERSION=1.6.0 $SOURCE_DIR/source/flatbuffers/build.sh
 ################################################################################
 (
   export BOOST_VERSION=1.57.0-p3
-  export KUDU_VERSION=6eb17c2
+  export KUDU_VERSION=${KUDU_VERSION:-6eb17c2}
   if $SOURCE_DIR/source/kudu/build.sh is_supported_platform; then
     $SOURCE_DIR/source/kudu/build.sh build
   else
